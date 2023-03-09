ac7637edf8143f3098db2a799aa7176131831fa0
Axojhf <20625305+axojhf@users.noreply.github.com>
2023-03-09 14:45:47 +0800

    add curl cpr test build

---
+++ fix_libcurl_pc.ps1
@@ -0,0 +1,13 @@
+$file = "C:\curl_install\lib\pkgconfig\libcurl.pc"
+$content = Get-Content $file
+$newContent = ""
+
+foreach ($line in $content) {
+    if ($line.StartsWith("supported_features=")) {
+        $features = $line.Substring(19) -replace '\s+', ';'
+        $line = "supported_features=$features"
+    }
+    $newContent += $line + "`n"
+}
+
+Set-Content $file $newContent.TrimEnd("`n")
