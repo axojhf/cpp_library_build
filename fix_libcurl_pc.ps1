38ce21d8a993497b214d6572a27d3718c2d62342
Axojhf <20625305+axojhf@users.noreply.github.com>
2023-03-29 14:05:43 +0800

    fix

---
+++ fix_libcurl_pc.ps1
@@ -0,0 +1,17 @@
+$file = "C:/curl/lib/pkgconfig/libcurl.pc"
+$content = Get-Content $file
+$newContent = ""
+
+foreach ($line in $content) {
+    if ($line.StartsWith("supported_features=")) {
+        $features = $line.Substring(19) -replace '\s+', ';'
+        $line = "supported_features=$features"
+    }
+    if ($line.StartsWith("supported_protocols=")) {
+        $features = $line.Substring(20) -replace '\s+', ';'
+        $line = "supported_protocols=$features"
+    }
+    $newContent += $line + "`n"
+}
+
+Set-Content $file $newContent.TrimEnd("`n")
